c2918f5144021a297dfd880abea728f4fc5dbc2e
joy-george <joy.g.arackal@gmail.com>
2019-02-17 21:48:00 +0530

    added feature to download and install redis package from source

--- cookbooks/install-redis/resources/custom_install.rb
+++ cookbooks/install-redis/resources/custom_install.rb
@@ -11,18 +11,22 @@ property :installation_type, String
 property :resource_directory, String
 
 action :install do
-  @download_directory = "#{node['custom_install']['download_directory']}/#{new_resource.custom_resource_name}/#{new_resource.version}"
-  action_create_directory
-  action_link_resource_directory
-
+  @download_directory = node['custom_install']['download_directory']
+  @install_directory = "#{@download_directory}/#{new_resource.custom_resource_name}-#{new_resource.version}"
   case new_resource.installation_type
   when 'git'
+    @download_directory = @install_directory
+    action_create_directory
     action_download_from_version_control
+
   else
+    action_create_directory
     action_download_from_package
+    action_uncompress_tar
   end
   action_build_package
   action_install_package
+  action_link_resource_directory
 end
 action :create_directory do
   directory @download_directory do
@@ -41,8 +45,23 @@ action :download_from_version_control do
   end
 end
 
-action :build_package do
+action :download_from_package do
+  remote_file "#{@download_directory}/#{new_resource.custom_resource_name}-#{new_resource.version}.tar.gz" do
+    source new_resource.url
+    action :create
+  end
+end
+
+action :uncompress_tar do
   working_dir = @download_directory
+  bash 'uncompress' do
+    user 'root'
+    cwd working_dir
+    code "tar -xvzf #{working_dir}/#{new_resource.custom_resource_name}-#{new_resource.version}.tar.gz"
+  end
+end
+action :build_package do
+  working_dir = @install_directory
   bash 'make' do
     user 'root'
     cwd working_dir
@@ -51,7 +70,7 @@ action :build_package do
 end
 
 action :install_package do
-  working_dir = @download_directory
+  working_dir = @install_directory
   bash 'make_install' do
     user 'root'
     cwd working_dir
@@ -60,7 +79,7 @@ action :install_package do
 end
 
 action :link_resource_directory do
-  working_dir = @download_directory
+  working_dir = @install_directory
   link new_resource.resource_directory do
     to working_dir
     action :create
--- cookbooks/install-redis/spec/unit/recipes/from-package_spec.rb
+++ cookbooks/install-redis/spec/unit/recipes/from-package_spec.rb
@@ -6,12 +6,12 @@
 
 require 'spec_helper'
 
-describe 'install-redis::from-repository' do
+describe 'install-redis::from-package' do
   context 'When all attributes are default, on Ubuntu 16.04' do
-    let(:installation_type) { 'git' }
-    let(:download_directory) { '/tmp/redis_git_2/redis-5.0.3' }
-    let(:repository) { 'https://github.com/antirez/redis.git' }
-    let(:revision) { '5.0' }
+    let(:download_directory) { '/tmp/redis_git_2' }
+    let(:install_directory) { "#{download_directory}/redis-5.0.3" }
+    let(:package_url) { 'http://download.redis.io/releases/redis-5.0.3.tar.gz' }
+    let(:version) { '5.0.3' }
     let(:redis_directory) { '/usr/local/share/redis' }
     let(:chef_run) do
       runner = ChefSpec::ServerRunner
@@ -20,13 +20,11 @@ describe 'install-redis::from-repository' do
                     version: '16.04')
       runner.converge(described_recipe)
     end
+
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-    it 'installs redis from repository' do
-      expect(chef_run).to custom_install('redis')
-        .with(installation_type: installation_type)
-    end
+
     it 'creates download directory' do
       expect(chef_run).to create_directory(download_directory)
         .with(owner: 'root',
@@ -34,23 +32,35 @@ describe 'install-redis::from-repository' do
               recursive: true)
     end
 
+    it 'downloads package from source' do
+      expect(chef_run).to create_remote_file("#{download_directory}/redis-#{version}.tar.gz")
+        .with(source: package_url)
+    end
+
+    it 'untar package' do
+      expect(chef_run).to run_bash('uncompress')
+        .with(cwd: download_directory,
+              user: 'root',
+              code: "tar -xvzf #{download_directory}/redis-#{version}.tar.gz")
+    end
+
     it 'build package' do
       expect(chef_run).to run_bash('make')
-        .with(cwd: download_directory,
+        .with(cwd: install_directory,
               user: 'root',
               code: 'make')
     end
 
     it 'install package' do
       expect(chef_run).to run_bash('make_install')
-        .with(cwd: download_directory,
+        .with(cwd: install_directory,
               user: 'root',
               code: 'make install')
     end
 
     it 'creates a link with redis directory' do
       expect(chef_run).to create_link(redis_directory)
-        .with(to: download_directory)
+        .with(to: install_directory)
     end
   end
 end
