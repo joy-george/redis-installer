31283138861bf3693a854f1fe239b766ad7642ad
joy-george <joy.g.arackal@gmail.com>
2019-02-19 16:51:34 +0530

    added redis user, group and configuration attributes

--- cookbooks/install-redis/attributes/default.rb
+++ cookbooks/install-redis/attributes/default.rb
@@ -6,3 +6,15 @@ default['redis']['source']['url'] = 'http://download.redis.io/releases/redis-5.0
 
 default['custom_install']['download_directory'] = '/custom_downloads'
 default['redis']['resource_directory'] = '/etc/redis'
+
+default['redis']['port'] = '6379'
+default['redis']['supervision_method'] = 'systemd'
+
+default['redis']['pidfile'] = "/var/run/redis_#{default['redis']['port']}.pid"
+default['redis']['db_directory'] = '/var/lib/redis'
+
+default['redis']['gid'] = 2001
+default['redis']['uid'] = 1001
+
+default['redis']['user'] = 'redis'
+default['redis']['group'] = 'redis'
